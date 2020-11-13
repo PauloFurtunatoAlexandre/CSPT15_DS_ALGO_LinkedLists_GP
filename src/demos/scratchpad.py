7ec107fa26a290607c87b2474eb2fecf3a2ec1e5
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-11-12 21:24:18 -0600

    added linked list example in Python

--- src/demos/scratchpad.py
+++ src/demos/scratchpad.py
@@ -1,3 +1,21 @@
 """
 Here we will outline a few concepts in comments and code.
 """
+
+
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+    def append(self, data):
+        new_node = LinkedListNode(data)
+
+        if self.head:
+            current = self.head
+
+            while current.next:
+                current = current.next
+
+            current.next = new_node
+         else:
+             self.head = new_node
